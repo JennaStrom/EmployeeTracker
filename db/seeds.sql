d023b95cf276d42cdca0ceabea664f3e4193434f
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-05-27 14:20:46 -0500

    Updated the indexjs and the seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,15 +3,28 @@ USE employee_db;
 INSERT INTO department 
 (name)
 VALUES 
-("Tech"),
-("Manager");
+("Engineering"),
+("Finance"),
+("Legal"),
+("Sales");
 
 INSERT INTO role 
 (title, salary, department_id)
 VALUES
-("worker", 80000, 1);
+("Lead Engineer", 100000, 1),
+("Software Engineer", 80000, 1),
+("Account Manager", 80000, 2),
+("Accountant", 70000, 2),
+("Legal Team", 90000, 3),
+("Sales Lead", 100000, 4),
+("Salesperson", 80000, 4),
+("Lawyer", 110000, 3);
 
 INSERT INTO employee
 (first_name, last_name, role_id, manager_id)
 VALUES
-("Jenna", "Strom", 1, NULL);
+("Jenna", "Capperton", 1, NULL),
+("Alex", "Smith", 1, 1),
+("Ben", "Johnson", 3, NULL),
+("Felix", "Hanson", 4, NULL),
+("Greg", "Miller", 4, 3);
--- index.js
+++ index.js
@@ -10,8 +10,32 @@ function prompt() {
             message: "What do you want to do?",
             choices: [
                 {
-                    name: "view all departments",
+                    name: "View all Departments",
                     value: "view_departments"
+                },
+                {
+                    name: "View all Employees",
+                    value: "view_employees"
+                },
+                {
+                    name: "View all Roles",
+                    value: "view_roles"
+                },
+                {
+                    name: "Add a Department",
+                    value: "add_department"
+                },
+                {
+                    name: "Add a Role",
+                    value: "add_role"
+                },
+                {
+                    name: "Add an Employee",
+                    value: "add_employee"
+                },
+                {
+                    name: "Update an Employee",
+                    value: "update_employee"
                 }
             ]
         }
@@ -21,9 +45,24 @@ function prompt() {
             case "view_departments":
                 viewDepartments();
                 break;
-            // case "view_roles":
-            //     viewRoles;
-            //     break;
+            case "view_employees":
+                viewEmployees();
+                break;
+            case "view_roles":
+                viewRoles();
+                break;
+            case "add_department":
+                addDepartment();
+                break;
+            case "add_role":
+                addRole();
+                break;
+            case "add_employee":
+                addEmployee();
+                break;
+            case "update_employee":
+                updateEmployee();
+                break;
             default: quit();
         }
     })
@@ -32,6 +71,34 @@ function viewDepartments() {
     return db.promise().query("SELECT * FROM department;").then(([rows]) => {
         let department = rows;
         console.table(department)
-    }).then(()=> prompt())
+    }).then(() => prompt())
+};
+
+function viewEmployees() {
+    return db.promise().query("SELECT * FROM employee").then(([rows]) => {
+        let employee = rows;
+        console.table(employee)
+    }).then(() => prompt())
+};
+
+function viewRoles() {
+    return db.promise().query("SELECT * FROM role").then(([rows]) => {
+        let role = rows;
+        console.table(role)
+    }).then(() => prompt())
+};
+
+function addDepartment() {
+    inquirer.prompt({
+        type: "input",
+        message: "What is the name of the department?",
+        name: "deptartment_name"
+    }).then(function (res) {
+        db.promise().query("INSERT INTO department (name) VALUES (?)", [res.dpartment_name], function (err, res) {
+            if (err) throw err;
+            console.table(res)
+        }).then(() => prompt())
+    })
 }
+
 prompt();
